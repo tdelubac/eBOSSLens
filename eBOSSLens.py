552184e5f231b671968098f7a6b7126cf6a15d5a
rameyer <romainalexis.meyer@epfl.ch>
2016-04-10 17:05:23 +0200

    Cleaning of the code, deleted useless functions

--- eBOSSLens.py
+++ eBOSSLens.py
@@ -31,29 +31,12 @@ def gauss(x,x_0,A,var):
 	y = A*n.exp( (-(x-x_0)**2) / (2*var) )
 	return y
 
-# The function whose square is to be minimised.
-# params = list of parameters tuned to minimise function.
-# Further arguments:
-# xdata = xaxis
-# ydata = observed data
-# yerr = variance of y
-def func(params, xdata, ydata, sqrtivar, x0, llimit, ulimit):
-	for i in n.arange(len(llimit)):
-		if ((params[i] < llimit[i]) or (params[i] > ulimit[i])):
-			return (ydata - gauss(x=xdata, x_0=x0, A=params[0], var=params[1]))*sqrtivar + 10 
-	return (ydata - gauss(x=xdata, x_0=x0, A=params[0], var=params[1]))*sqrtivar
-
-def func2(params, xdata, ydata, sqrtivar, x0, llimit, ulimit):
-	if ( not(10 > abs(params[3]) > 1.3) or  not(ulimit[0]>params[0] > llimit[0]) or not(ulimit[1]>params[1] > llimit[1]) or not(ulimit[0] > params[2] > llimit[0]) ):
-			return (ydata - gauss(x=xdata, x_0=x0, A=params[0], var=params[1])-gauss(x=xdata, x_0=x0-params[3], A=params[2], var=params[1]))*sqrtivar + 10 
-	return (ydata - gauss(x=xdata, x_0=x0, A=params[0], var=params[1])-gauss(x=xdata, x_0=x0-params[3], A=params[2], var=params[1]))*sqrtivar
-
+#Chi square for one gaussian
 def chi2g(params, xdata, ydata, ivar, x0):
 	return sum(ivar*(ydata - gauss(x=xdata, x_0=x0, A=params[0], var=params[1]))**2)/(len(xdata)-len(params)-1)
-
-
-def chi2(params, xdata, ydata, ivar, x0):
-	return sum(ivar*(ydata - gauss(x=xdata, x_0=x0, A=params[0], var=params[1])-gauss(x=xdata, x_0=x0-params[3], A=params[2], var=params[1]))**2)/(len(xdata)-len(params) -1)
+#Chi square for Doublet
+def chi2D(params, xdata, ydata, ivar):
+	return sum(ivar*(ydata - gauss(x=xdata, x_0=params[3], A=params[0], var=params[1])-gauss(x=xdata, x_0=params[4], A=params[2], var=params[1]))**2)/(len(xdata)-len(params) -1)
 
 # Check if x0 is near any emission line redshifted by z
 def nearline(x0, zline, fiberid, z, mjd, plate):
@@ -65,15 +48,13 @@ def nearline(x0, zline, fiberid, z, mjd, plate):
 		return True
 	else:
 		return False
-		
+
+#Gaussian kernel used in first feature search (Bolton et al.,2004 method)	
 def kernel(j,width,NormGauss,length):
 	ker = n.zeros(length)
 	ker[j-int(width*0.5):j+int(width*0.5)] = NormGauss
 	return ker
 	
-def gauss2chi2(params, xdata, ydata, ivar, x0):
-	return sum(ivar*(ydata - gauss(x=xdata, x_0=params[3], A=params[0], var=params[1])-gauss(x=xdata, x_0=params[4], A=params[2], var=params[1]))**2)/(len(xdata)-len(params) -1)
-
 # Check if a path exists, if not make it
 def make_sure_path_exists(path):
     try:
@@ -191,7 +172,6 @@ for j in n.arange(len(plate_mjd)):
 		if (obj_class[i] == 'STAR  ' or obj_class[i] == 'QSO   '): # or zwarning[i]!=0):
 			continue
 		peaks = []
-		peaks_err = []
 		peak_number = len(peaks)
 		searchpeaks = True
 		doublet = None
@@ -210,18 +190,14 @@ for j in n.arange(len(plate_mjd)):
 		SN = n.zeros(len(wave))
 		SN[width*0.5:len(wave)-width*0.5] = Cj1/n.sqrt(Cj2)
 		
-		
-		print 'Fiber ', fiberid[i] , 'just sorted all potential peaks' 
+		peak_candidates = n.array([(x0,0.0,0.0,0.0,test,0.0,0.0,0.0,0.0,0.0,0.0) for x0,test in zip(wave,SN) if test>6.0])
+		# Legend key x0: wavelength(x0) chisq_doublet amp_gauss var_gauss S/N chisq_doublet amp1_doublet amp2_doublet var_doublet x1 x2
 
-		#peak_candidates = n.array([(x0,0.0,0.0,0.0,0.0,0.0,test,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0) for x0,test in zip(wave,reduced_flux[i,:]* sqrtivar[i,:]) if test>5])
-		peak_candidates = n.array([(x0,0.0,0.0,0.0,0.0,0.0,test,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0) for x0,test in zip(wave,SN) if test>6.0])
-		# Legend key x0: wavelength chisq_doublet amp_gauss var_gauss err_amp err_var S/N chisq_doublet amp1_doublet amp2_doublet var_doublet err1_doublet err2_doublet err_var_doublet
-
-		#Keep only center of candidate peak
+		#Keep only center of candidate peaks
 		k = 0
 		while (k < (len(peak_candidates)-1)):
 			if (abs(peak_candidates[k][0] - peak_candidates[k+1][0]) < 10):
-				if peak_candidates[k][6] < peak_candidates[k+1][6]:
+				if peak_candidates[k][4] < peak_candidates[k+1][4]:
 					peak_candidates = n.delete(peak_candidates,k, axis=0)
 					k = k-1
 				else:
@@ -234,59 +210,51 @@ for j in n.arange(len(plate_mjd)):
 			x0 = peak[0]
 			if nearline(x0, zline, fiberid[i], z[i], int(mjd), int(plate)):
 				continue
-			cov=0
 			#Single Line: Gaussian fit around x_0
-			init = [2,2]
-			params, cov, infodict, mesg, ier = leastsq(func, init,
-				 args=(wave, reduced_flux[i,:], sqrtivar[i,:], x0, llimit, ulimit),
-				 full_output=True)
-			res =  minimize(chi2g,init,args=(wave, reduced_flux[i,:],ivar[i,:],x0), method='SLSQP', bounds = [(0.2,10),(1,8)])
+			init = [1,2]
+			res =  minimize(chi2g,init,args=(wave, reduced_flux[i,:],ivar[i,:],x0), method='SLSQP', bounds = [(0.1,5),(1,8)])
 			params = res.x
 			residue_squared=((reduced_flux[i,:] - gauss(x=wave, x_0=x0, A=params[0], var=params[1]))**2)*ivar[i,:]
 			chisq =  sum(residue_squared)/(len(wave)-len(params) -1)
 			chigauss1 = chisq
-			
-			#Check for S/N > 4 fit
-			if not(cov is None):
+			#Check for not to high chi square and save
+			if not(chisq > 2.0):
 				peak[1] = chisq
 				peak[2] = params[0]
 				peak[3] = params[1]
-				peak[4] = cov[0,0]
-				peak[5] = cov[1,1]	
 				
 			#Doublet OII: Gaussian fit around x_0
 			if (x0 > 3727.0*(1+z[i])):
 
-				res2 = minimize(gauss2chi2,[1.0,5,1.0,x0-1.5,x0+1.5],args=(wave, reduced_flux[i,:],ivar[i,:],x0), method='SLSQP', bounds = [(0.1,3),(1,8),(0.1,3),(x0-7,x0),(x0,x0+7)])
+				res2 = minimize(chi2D,[1.0,5,1.0,x0-1.5,x0+1.5],args=(wave, reduced_flux[i,:],ivar[i,:]), method='SLSQP', bounds = [(0.1,5),(1,8),(0.1,5),(x0-7,x0),(x0,x0+7)])
 				params2 = res2.x
 				residue_squared=((reduced_flux[i,:] - gauss(x=wave, x_0=params2[3], A=params2[0], var=params2[1]) - gauss(x=wave, x_0=params2[4], A=params2[2], var=params2[1]))**2)*ivar[i,:]
 				chisq2 = sum(residue_squared)/(len(wave)-len(params2) -1)
 				
 				if abs(params2[3]-params2[4])>1.5:	
-					peak[7] = chisq2
-					peak[8] = params2[0] #amp1
-					peak[9] = params2[1] #var
-					peak[10] = params2[2] #amp2
-					peak[11] = params2[3] #x1
-					peak[12] = params2[4] #x2
-					peak[13] = 1.0#cov[2,2] #err var
-					peak[14] = 1.0#cov[1,1] #err amp2
-		print peak_candidates				
+					peak[5] = chisq2
+					peak[6] = params2[0] #amp1
+					peak[7] = params2[2] #amp2
+					peak[8] = params2[1] #var
+					peak[9] = params2[3] #x1
+					peak[10] = params2[4] #x2
+					
+				
 		#Finding peak with lowest chi square for doublet and see if it is better fitted by single line or not
 		doublet_index = 0
 		chi2saved = 1000.0
 		# Find the doublet index
 		for k in range(len(peak_candidates)):
 			peak = peak_candidates[k]
-			if (peak[1]>peak[7]>0 and peak[7]< chi2saved):
-				peak[1] = peak[7]
-				chi2saved = peak[7]
+			if (peak[1]>peak[5]>0 and peak[5]< chi2saved):
+				peak[1] = peak[5]
+				chi2saved = peak[5]
 				doublet = True
 				doublet_index = k		
 		
 		
 		#Removing candidates that were not fitted : params still 0
-		peak_candidates = n.array([peak for peak in peak_candidates if (peak[2]!=0 or peak[7]==peak[1]!=0)])
+		peak_candidates = n.array([peak for peak in peak_candidates if (peak[2]!=0 or peak[5]==peak[1]!=0)])
 		if len(peak_candidates) == 0:
 			continue
 		
@@ -299,7 +267,7 @@ for j in n.arange(len(plate_mjd)):
 		#find again doublet index
 		found = False
 		for k in range(len(peak_candidates)):
-			if (peak_candidates[k][7] == chi2saved):
+			if (peak_candidates[k][5] == chi2saved):
 				doublet_index = k
 				found = True
 		if found==False:
@@ -322,7 +290,7 @@ for j in n.arange(len(plate_mjd)):
 				detection = True
 				print '(Doublet) Lensed object at z1 = ', z_background 
 			if len(peak_candidates):	
-				temp = [peak for peak in peak_candidates if peak[1]!=peak[7]]
+				temp = [peak for peak in peak_candidates if peak[1]!=peak[5]]
 				compare = em_lines[1:5]
 				if z_background > z[i]+0.05 :
 					for peak in temp:
@@ -348,20 +316,15 @@ for j in n.arange(len(plate_mjd)):
 			#Redefine variables for clarity
 			chisq_saved = peak[1]
 			params = [peak[2],peak[3]] #amplitude, variance
-			err_amp = peak[4]
-			err_var = peak[5]
 			if (k == doublet_index and doublet ==True):
 				# Save doublet gaussians
-				peaks.append([peak[11], peak[8], peak[9]])
-				peaks_err.append([math.sqrt(peak[12]*chisq_saved), math.sqrt(peak[13]*chisq_saved)])
-				peaks.append([peak[12], peak[10], peak[9]])
-				peaks_err.append([math.sqrt(peak[14]*chisq_saved), math.sqrt(peak[13]*chisq_saved)])
+				peaks.append([peak[9], peak[6], peak[8]])
+				peaks.append([peak[10], peak[7], peak[8]])
 			else:
 				# Save emission line 
 				peaks.append([x0_saved, params[0], params[1]])
-				peaks_err.append([math.sqrt(err_amp*chisq_saved), math.sqrt(err_var*chisq_saved)])
 			# Set ivar = 0 for points around peak/emission line found
-			delta = 2*int((math.log(1 + math.sqrt(params[1])/x0_saved)/math.log(10)) / c1)
+			delta = 2*int((math.log(1 + math.sqrt(params[1])/x0_saved)/math.log(10)) /c1)
 			if (delta == 0):
 				delta=1
 			center = int(((math.log(x0_saved)/math.log(10))-c0)/c1)
@@ -384,12 +347,9 @@ for j in n.arange(len(plate_mjd)):
 			fit=0
 			for k in n.arange(len(peaks)):
 				detected.append([RA[i], DEC[i], int(plate), int(mjd), fiberid[i],
-						peaks[k][0], peaks[k][1], math.sqrt(peaks[k][2]), peaks_err[k][0], n.sqrt(peaks_err[k][1]), peak_number])
+						peaks[k][0], peaks[k][1], math.sqrt(peaks[k][2]), peak_number])
 				fit = fit + gauss(wave, x_0 = peaks[k][0] , A=peaks[k][1], var=peaks[k][2])
-				#p.annotate(str(int(peaks[k][1]/peaks_err[k][0]))+' sig \n|',
-				#		xy=(peaks[k][0], reduced_flux[i,int(((math.log(peaks[k][0])/math.log(10))-c0)/c1)]),
-				#		xytext=(peaks[k][0], reduced_flux[i,int(((math.log(peaks[k][0])/math.log(10))-c0)/c1)] + 3),
-				#		ha='center', fontsize=10)
+
 			p.plot(wave,fit,'r',hold=True)
 			ax.set_ylim(ymin = -0.5, ymax = 5)
 			print ' '*60, '\r', "saving figure\r",
@@ -410,7 +370,7 @@ for j in n.arange(len(plate_mjd)):
 			f.close()
 	
 	print 'Time taken ', (datetime.datetime.now() - startTime)
-	detected = sorted(detected, key = lambda obj : obj[10], reverse = True)
+	detected = sorted(detected, key = lambda obj : obj[8], reverse = True)
 
 f = open(topdir + '/candidates.txt','a')
 f.write('\n Doublet: ' + str(countDoublet) + ' D+M: ' + str(countDM) + ' Multi: ' + str(countMulti)	)

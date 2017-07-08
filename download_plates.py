c08f889ca918bb398606c543fa6e31026651e272
chengxinlun <chengxinlun@gmail.com>
2017-07-08 13:43:25 +0200

    1. Create gitignore
    2. Update download_plates.py for more freedom
    
    3. Bug fix eBOSSLens.py: index not an int

--- download_plates.py
+++ download_plates.py
@@ -1,27 +1,61 @@
+import os
+import urlparse
 import urllib
-import pyfits as pf
 import numpy as n
 
 
-dir = "http://data.sdss3.org/sas/dr10/sdss/spectro/redux/v5_5_12/"
+def create_directory(dire):
+    '''
+    create_directory(dire)
+    ======================
+    Input: dire: a relative directory that needs to be created
+    Output: None
+    '''
+    direlast = os.path.dirname(dire)
+    if not os.path.exists(direlast):
+        os.makedirs(direlast)
 
-# Download_list.txt should be written plain as two columns: "plate_number" "mjd"
 
-plate_mjd = [line.strip().split() for line in open('download_list.txt')]
+def download_plates(is570):
+    # Download_list.txt should be written plain as two columns:
+    # "plate_number" "mjd"
+    plate_mjd = [line.strip().split() for line in open('download_list.txt')]
+    # Update to dr12
+    # Add 5.7.0 and 5.7.2
+    if is570:
+        flag = "v5_7_0"
+    else:
+        flag = "v5_7_2"
+    baseDir = "../SCRATCH/BOSS/data/" + flag
+    baseUrl = "http://data.sdss3.org/sas/dr12/sdss/spectro/redux/" + flag + "/"
+    for i in n.arange(len(plate_mjd)):
+        # Construct file
+        fileSfx = plate_mjd[i][0] + "-" + plate_mjd[i][1] + ".fits"
+        sppFile = plate_mjd[i][0] + "/spPlate-" + fileSfx
+        spbFile = plate_mjd[i][0] + "/" + flag + "/spZbest-" + fileSfx
+        splFile = plate_mjd[i][0] + "/" + flag + "/spZline-" + fileSfx
+        # Construct saving directories
+        sppDir = os.path.join(baseDir, sppFile)
+        spbDir = os.path.join(baseDir, spbFile)
+        splDir = os.path.join(baseDir, splFile)
+        create_directory(sppDir)
+        create_directory(spbDir)
+        create_directory(splDir)
+        # Construct urls
+        sppUrl = urlparse.urljoin(baseUrl, sppFile)
+        spbUrl = urlparse.urljoin(baseUrl, spbFile)
+        splUrl = urlparse.urljoin(baseUrl, splFile)
+        # download spPlate
+        urllib.URLopener()
+        print("Downloading " + str(sppUrl))
+        urllib.urlretrieve(sppUrl, sppDir)
+        # download spZbest
+        print("Downloading " + str(spbUrl))
+        urllib.urlretrieve(spbUrl, spbDir)
+        # download spZline
+        print("Downloading " + str(splUrl))
+        urllib.urlretrieve(splUrl, splDir)
 
-			
-for i in n.arange(len(plate_mjd)):
-	print "Downloading spPlate ", plate_mjd[i][1], "\r",
-	# download spPlate
-	u = urllib.URLopener()
-	urllib.urlretrieve( dir + plate_mjd[i][0] + "/spPlate-" + plate_mjd[i][0] + "-"+ plate_mjd[i][1] + ".fits", "./fits_files/spPlate-" + plate_mjd[i][0] + "-"+ plate_mjd[i][1] + ".fits")
 
-	print "Downloading spZbest ", plate_mjd[i][0], "\r",
-	# download spZbest
-	urllib.urlretrieve( dir + plate_mjd[i][0] + "/v5_5_12/spZbest-" + plate_mjd[i][0] + "-" + plate_mjd[i][1] + ".fits", "./fits_files/spZbest-" + plate_mjd[i][0] + "-"+ plate_mjd[i][1] + ".fits")
-
-	print "Downloading spZline ", plate_mjd[i][0], "\r",
-	# download spZline
-	urllib.urlretrieve( dir + plate_mjd[i][0] + "/v5_5_12/spZline-" + plate_mjd[i][0] + "-"+ plate_mjd[i][1] + ".fits", "./fits_files/spZline-" + plate_mjd[i][0] + "-"+ plate_mjd[i][1] + ".fits")
-
-	
+if __name__ == "__main__":
+    download_plates(True)

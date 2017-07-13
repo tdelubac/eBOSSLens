077dec15a9a10a2c537264baf1b4dcfe7018d972
chengxinlun <chengxinlun@gmail.com>
2017-07-13 13:37:01 +0200

    Parallel implimentation

--- main.py
+++ main.py
@@ -1,9 +1,31 @@
 # import argparse
 import os
+import multiprocessing as mul
 import numpy as np
 from eBOSSLens import eBOSSLens
 from utils import make_sure_path_exists
 
+
+def para_return(func, params, num_thread=4):
+    res_list = []
+    p = mul.Pool(processes=num_thread)
+    for each in params:
+        def _log_result(res):
+            res_list.append(res)
+        p.apply_async(func, each, callback=_log_result)
+    p.close()
+    p.join()
+    return res_list
+
+
+def lensFinder(plate, mjd, fiberid, datav='v5_7_0', datadir='/SCRATCH'):
+    savedir = os.path.join('../FullSearch', str(plate) + "-" + str(mjd))
+    make_sure_path_exists(savedir)
+    try:
+        eBOSSLens(plate, mjd, each, datav, False, False, False, savedir,
+                  datadir)
+    except Exception as reason:
+        print(reason)
 
 if __name__ == "__main__":
     '''
@@ -20,16 +42,10 @@ if __name__ == "__main__":
     args = parser.parse_args()
     eBOSSLens(args.plate, args.mjd, args.fiberid, False, False, False)
     '''
-    plate = 4391
-    mjd = 55866
-    fiberid = np.arange(1, 1001, 1, dtype=int)
-    datav = 'v5_7_0'
-    datadir = '../SCRATCH'
-    savedir = os.path.join('../FullSearch', str(plate) + "-" + str(mjd))
-    make_sure_path_exists(savedir)
-    for each in fiberid:
-        try:
-            eBOSSLens(plate, mjd, each, datav, False, False, False, savedir,
-                      datadir)
-        except Exception as reason:
-            print(str(each) + " " + str(reason))
+    platemjd = np.loadtxt('plate-mjd.txt')
+    for each in platemjd:
+        fiberid = np.arange(1, 1001, 1, dtype=int)
+        args = []
+        for fid in fiberid:
+            args.append((each[0], each[1], fid,))
+        res = para_return(lensFinder, args, 16)

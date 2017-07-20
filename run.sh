207a44f9b1786de45e279686005f1c187bc3456d
chengxinlun <chengxinlun@gmail.com>
2017-07-20 19:24:36 +0200

    Fix up run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
 #SBATCH -p r3 -o my.stdout -e my.stderr --mail-user=xinlun.cheng@epfl.ch --mail-type=ALL
-python main.py
+python main.py plate-mjd.txt -c 3.5 --savedir ../FullSearch/3.5
+python main.py plate-mjd.txt -c 3.0 --savedir ../FullSearch/3.0

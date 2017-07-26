71d6a876fe3dbd5f84a48930a6e26880aa804a2d
rameyer <romainalexis.meyer@epfl.ch>
2017-07-26 16:30:34 +0200

    Added self-explanatory exceptions instead of return False statements

--- objFilter.py
+++ objFilter.py
@@ -18,10 +18,10 @@ def qsoFilter(obj, DR12Q, rchi2_max):
     if obj.obj_type[i].startswith('sky') or \
             obj.obj_type[i].startswith('SPECTROPHOTO_STD') or \
             (not obj.obj_class.startswith('QSO') ):
-        continue
+        raise Exception("Rejected: Not a proper QSO target")
     # Reject badly fitted QSOs in BOSS pipeline
     if obj.zwarning[i] != 0 or obj.rchi2[i] > rchi2_max or obj.z_err[i] < 0:
-        continue
+        raise Exception("Rejected: Bad redsfhit fitting")
     # Search for matching QSOs in DR12Q
     matching_QSOs = [x for x in DR12Q if int(x[0]) == int(self.plate) and
              int(x[1]) == int(self.mjd) and int(x[2]) == int(self.fid)]
@@ -31,17 +31,17 @@ def qsoFilter(obj, DR12Q, rchi2_max):
                 n.abs(index[0][4] - obj.z[i]) > 0.1:
             continue
     else:
-        return False
+        raise Exception("Rejected: .fits redshift does not match DR12Q tabulated redshift")
 
     # Before masking, compute the FWHM of CIV or HBeta depending on redshift:
-    FWHM, l_times_luminosity, HB_wave, params_beta, line_coeff = \
-        QSO_compute_FWHM(ivar = ivar[i,:],flux = flux[i,:], wave = wave,c0=c0,c1=c1,Nmax=Nmax,z =z[i],l_width = l_width)
+    #FWHM, l_times_luminosity, HB_wave, params_beta, line_coeff = \
+    #    QSO_compute_FWHM(ivar = ivar[i,:],flux = flux[i,:], wave = wave,c0=c0,c1=c1,Nmax=Nmax,z =z[i],l_width = l_width)
 
-    M_BH = 10**(6.91 + n.log10(n.sqrt(5100*l_times_luminosity/1e44)*(FWHM/1000)**2)) # Masses solaires
-    sigma_host = 200*10**((n.log10(M_BH) - 7.92)/3.93) ### km s-1
+    #M_BH = 10**(6.91 + n.log10(n.sqrt(5100*l_times_luminosity/1e44)*(FWHM/1000)**2)) # Masses solaires
+    #sigma_host = 200*10**((n.log10(M_BH) - 7.92)/3.93) ### km s-1
 
 
-    ivar[i,:] = mask_QSO(ivar=ivar[i,:],z=z[i], l_width = l_width, c0 = c0 , c1=c1,Nmax=Nmax)
+    #ivar[i,:] = mask_QSO(ivar=ivar[i,:],z=z[i], l_width = l_width, c0 = c0 , c1=c1,Nmax=Nmax)
 
     return True
 
@@ -60,6 +60,6 @@ def genFilter(obj):
     if obj.obj_class.startswith('STAR') or obj.obj_class.startswith('QSO') or \
                     obj.obj_type.startswith('SKY') or \
                     obj.obj_type.startswith('SPECTROPHOTO_STD'):
-        return False
+        raise Exception("Rejected: Not a proper galaxy target")
     else:
         return True
